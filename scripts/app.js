591f1a82fa31dcdc5c91dcebce23665487e4f451
soumya <soumya@example.com>
2024-09-21 21:32:42 +0530

    Added responsive footer with custom styling and Scroll-to-top' feature by javascript

---
+++ scripts/app.js
@@ -0,0 +1,5 @@
+// When the user clicks back to top the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
