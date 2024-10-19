c5cd668497dd06be4742e7fbabaa63a5a2ba305c
soumya <soumya@example.com>
2024-10-19 19:13:46 +0530

    fix the bugs in featutes section

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,74 @@
-// banner section 
+// Header Section
+const gift = document.querySelector("div.gift");
+const dropdown = document.querySelector("div.gift .dropdown");
+const drop_arrow = document.querySelector("#drop_arrow");
+const toggleDropdown = () => {
+  gift.style.outline = "3px solid #f90";
+  if (dropdown.style.display == "none") dropdown.style.display = "block";
+  else {
+    dropdown.style.display = "none";
+  }
+};
+gift.addEventListener("click", toggleDropdown);
+drop_arrow.addEventListener("click", toggleDropdown);
+document.addEventListener("click", () => {
+  console.log(event.target);
+  if (event.target !== gift && event.target !== drop_arrow) {
+    gift.style.outline = "none";
+    dropdown.style.display = "none";
+  }
+});
+const input = document.querySelector("input");
+const search = document.querySelector(".search");
+const main = document.querySelector("main");
+const footer = document.querySelector("footer");
+const cover = document.querySelector(".cover");
+let main_height = main.offsetHeight;
+let footer_height = footer.offsetHeight;
+let cover_height = main_height + footer_height;
+console.log(cover_height);
+input.addEventListener("focus", () => {
+  search.style.outline = "3px solid #f90";
+  cover.style.height = `${cover_height}px`;
+});
+input.addEventListener("blur", () => {
+  search.style.outline = "none";
+  cover.style.height = 0;
+});
+// banner section
 const banner_path = "./image/";
-const banner_slider = [{ banner_name: "books_banner", banner_url: banner_path + "books_banner.jpg" }, { banner_name: "beauty_banner", banner_url: banner_path + "beauty_banner.jpg" }, { banner_name: "toys_banner", banner_url: banner_path + "toys_banner.jpg" }, { banner_name: "kitchen_banner", banner_url: banner_path + "kitchen_banner.jpg" }, { banner_name: "game_banner", banner_url: banner_path + "game_banner.jpg" }];
-let banner_section = document.querySelector("section.banner");
+const banner_slider = [
+  { banner_name: "books_banner", banner_src: banner_path + "books_banner.jpg" },
+  {
+    banner_name: "beauty_banner",
+    banner_src: banner_path + "beauty_banner.jpg",
+  },
+  { banner_name: "toys_banner", banner_src: banner_path + "toys_banner.jpg" },
+  {
+    banner_name: "kitchen_banner",
+    banner_src: banner_path + "kitchen_banner.jpg",
+  },
+  {
+    banner_name: "shop_catagories_banner.jpg",
+    banner_src: banner_path + "shop_catagories_banner.jpg",
+  },
+  { banner_name: "game_banner", banner_src: banner_path + "game_banner.jpg" },
+];
+
+let banner_section = document.querySelector("a.slides img");
 let i = 0;
 setInterval(() => {
-
   while (i < banner_slider.length) {
-    banner_section.style.backgroundImage = `url(${banner_slider[i].banner_url})`;
-    console.log(i);
+    banner_section.src = `${banner_slider[i].banner_src}`;
+    banner_section.alt = `${banner_slider[i].banner_name}`;
+    // console.log(i);
     i++;
     if (i === banner_slider.length) {
-      i = 0
+      i = 0;
     }
     break;
   }
-}, 3000)
+}, 3000);
 // When the user clicks back to top the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0;
